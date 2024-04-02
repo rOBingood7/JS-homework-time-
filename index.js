98179113375c22bd15fb0f00f4d161507b032059
rOBingood7 <mirkomilmuhtorov22@gmail.com>
2024-04-02 16:24:49 +0500

    homework

---
+++ index.js
@@ -0,0 +1,82 @@
+/* let localTime = new Date()
+let hour = localTime.getHours()
+
+let greeting
+
+if (hour >= 0 && hour < 6) {
+    greeting = 'Доброй ночи!'
+} else if (hour >= 6 && hour < 12) {
+    greeting = 'Доброе утро!'
+} else if (hour >= 12 && hour < 18 ) {
+    greeting = 'Добрый день!'
+} else {
+    greeting = 'Добрый вечер'
+}
+ alert(greeting) */
+
+
+
+
+
+/* let age = +prompt('What is your age?')
+
+let ageMeaning
+
+if (age >= 0 && age < 2) {
+    ageMeaning = 'baby'
+} else if (age >= 2 && age < 11) {
+    ageMeaning = 'child'
+} else if (age >= 11 && age < 18) {
+    ageMeaning = 'teen'
+} else if (age >= 18 && age < 40) {
+    ageMeaning = 'adult'
+} else if (age >= 40 && age < 60) {
+    ageMeaning = 'middle age adult'
+} else if (age >= 60) {
+    ageMeaning = 'senior adult'
+} 
+ 
+alert(ageMeaning)
+ */
+
+
+
+
+
+
+/* let monthNumber = +prompt('What month is it? (Number only)')
+
+let season 
+
+if (monthNumber === 12 || monthNumber >= 1 && monthNumber <= 2) {
+    season = 'Winter'
+} else if (monthNumber >= 3 && monthNumber <= 5) {
+    season = 'Spring'
+} else if (monthNumber >= 6 && monthNumber <= 8) {
+    season = 'Summer'
+} else if (monthNumber >= 9 && monthNumber <= 11) {
+    season = 'Autumn'
+} else (
+    season = 'There is no such month'
+)
+
+
+alert(season) */
+
+
+
+/* let decade = +prompt('What date is it today?')
+
+if (decade > 0 && decade <= 10) {
+    num = 1
+} else if (decade > 10 && decade <= 20) {
+    num = 2
+} else if (decade > 20 && decade <= 30) {
+    num = 3
+} else if (decade === 31) {
+    num = 4
+} else (
+    num = undefined
+)
+
+alert(num) */
